10d73201f29660260c873d141f0c403ea6d5b691
Elon Aseneka <elonsantos63@gmail.com>
2022-04-26 18:23:21 +0300

    final touches

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,142 +1,64 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
+import Axios from 'axios';
 
 interface BlogProps {
     
 }
  
 const Blog: FunctionComponent<BlogProps> = () => {
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        try {
+          const res = await Axios.get(
+            `https://www.googleapis.com/blogger/v3/blogs/${process.env.REACT_APP_BLOGGER_BLOG_ID}/posts?key=${process.env.REACT_APP_BLOGGER_API_KEY}&fetchImages=true`
+          );
+    
+          console.log(res.data);
+          
+          setPosts(res.data.items);
+        //   setPosts(res.data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      useEffect(() => {
+        fetchPosts();
+      }, []);
+    
     return (
         <>
         <section id="section-blog" className="dark">
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12 wow fadeInRight">
-                                <h4 className="title">This is my story</h4>
+                                <h4 className="title">These are my articles</h4>
                                 <h2 className="title">My Blog</h2>
                                 <div className="title-underline"></div>
                             </div>
                         </div>
                         <div className="row wow fadeInRight" data-wow-delay=".25s">
-                            <div className="col-lg-4 col-md-6 mb30">
+                            {posts.map((post: any) => <div key={post.id} className="col-lg-4 col-md-6 mb30">
                                 <div className="bloglist item">
                                     <div className="post-content">
                                         <div className="post-image d-hover-zoom">
-                                            <a className="image-popup" href="images/news/1.jpg">
-                                                <img alt="" src="images/news/1.jpg" />
+                                            <a className="image-popup" href={post?.url} rel="noreferrer" target="_blank">
+                                                <img alt="" src={post?.images[0].url} />
                                             </a>
                                             <div className="post-info">
                                                 <div className="inner">
-                                                    <span className="post-date">10 Dec 2020</span>
+                                                    <span className="post-date">{`${new Date(post?.published)}`}</span>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="post-text">
-                                            <h4><a href="blog-single.html">Better User Interface</a></h4>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                            <h4><a href={post?.url} rel="noreferrer" target="_blank">{post?.title}</a></h4>
+                                            {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6 mb30">
-                                <div className="bloglist item">
-                                    <div className="post-content">
-                                        <div className="post-image d-hover-zoom">
-                                            <a className="image-popup" href="images/news/2.jpg">
-                                                <img alt="" src="images/news/2.jpg" />
-                                            </a>
-                                            <div className="post-info">
-                                                <div className="inner">
-                                                    <span className="post-date">10 Dec 2020</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="post-text">
-                                            <h4><a href="blog-single.html">Experts Web Design Tips</a></h4>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6 mb30">
-                                <div className="bloglist item">
-                                    <div className="post-content">
-                                        <div className="post-image d-hover-zoom">
-                                            <a className="image-popup" href="images/news/3.jpg">
-                                                <img alt="" src="images/news/3.jpg" />
-                                            </a>
-                                            <div className="post-info">
-                                                <div className="inner">
-                                                    <span className="post-date">10 Dec 2020</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="post-text">
-                                            <h4><a href="blog-single.html">Importance Of Web Design</a></h4>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6 mb30">
-                                <div className="bloglist item">
-                                    <div className="post-content">
-                                        <div className="post-image d-hover-zoom">
-                                            <a className="image-popup" href="images/news/4.jpg">
-                                                <img alt="" src="images/news/4.jpg" />
-                                            </a>
-                                            <div className="post-info">
-                                                <div className="inner">
-                                                    <span className="post-date">10 Dec 2020</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="post-text">
-                                            <h4><a href="blog-single.html">Avoid These Erros In UI Design</a></h4>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6 mb30">
-                                <div className="bloglist item">
-                                    <div className="post-content">
-                                        <div className="post-image d-hover-zoom">
-                                            <a className="image-popup" href="images/news/5.jpg">
-                                                <img alt="" src="images/news/5.jpg" />
-                                            </a>
-                                            <div className="post-info">
-                                                <div className="inner">
-                                                    <span className="post-date">10 Dec 2020</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="post-text">
-                                            <h4><a href="blog-single.html">Make Mobile Website Faster</a></h4>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 col-md-6 mb30">
-                                <div className="bloglist item">
-                                    <div className="post-content">
-                                        <div className="post-image d-hover-zoom">
-                                            <a className="image-popup" href="images/news/6.jpg">
-                                                <img alt="" src="images/news/6.jpg" />
-                                            </a>
-                                            <div className="post-info">
-                                                <div className="inner">
-                                                    <span className="post-date">10 Dec 2020</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="post-text">
-                                            <h4><a href="blog-single.html">How Sell Digital Product</a></h4>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            </div>)}
                         </div>
                     </div>
                 </section>
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -19,69 +19,19 @@ const Portfolio: FunctionComponent<PortfolioProps> = () => {
                             <div className="col-md-12 text-center wow fadeInRight" data-wow-delay=".25s">
                                 <ul id="filters">
                                     <li><a href="#" data-filter="*" id="btn_all" className="selected">all projects</a></li>
-                                    <li><a href="#" data-filter=".illustration">illustration</a></li>
-                                    <li><a href="#" data-filter=".mobile">mobile</a></li>
-                                    <li><a href="#" data-filter=".photography">photography</a></li>
-                                    <li><a href="#" data-filter=".website">website</a></li>
+                                    <li><a href="#" data-filter=".react">React</a></li>
+                                    <li><a href="#" data-filter=".node">Node.js/NestJS</a></li>
+                                    <li><a href="#" data-filter=".angular">Angular</a></li>
+                                    <li><a href="#" data-filter=".reactnative">React Native</a></li>
                                 </ul>
                                 <div id="gallery" className="gallery full-gallery de-gallery zoom-gallery row sequence wow fadeInRight" data-wow-delay=".5s">
                                     {/* gallery item */}
-                                    <div className="item col-md-4 mb30 gallery-item illustration website" data-value="project-details-image.html">
-                                        <a className="picframe" href="images/portfolio/1.jpg" title="Exhibiz" data-source="https://themeforest.net/item/exhibiz-event-conference-and-meetup/28663470">
+                                    <div className="item col-md-4 mb30 gallery-item angular" data-value="project-details-image.html">
+                                        <a className="picframe" href="images/portfolio/1.jpg" title="Ardhisasa" data-source="https://ardhisasa.lands.go.ke">
                                             <span className="overlay">
-                                                <span className="d-title">Exhibiz</span>
+                                                <span className="d-title">Ardhisasa</span>
                                             </span>
                                             <img src="images/portfolio/1.jpg" alt="" />
-                                        </a>
-                                    </div>
-                                    {/* close gallery item */}
-                                    {/* gallery item */}
-                                    <div className="item col-md-4 mb30 gallery-item photography">
-                                        <a className="picframe" href="images/portfolio/2.jpg" title="Bolo" data-source="https://themeforest.net/item/bolo-onepage-creative-website-template/25030305">
-                                            <span className="overlay">
-                                                <span className="d-title">Bolo</span>
-                                            </span>
-                                            <img src="images/portfolio/2.jpg" alt="" />
-                                        </a>
-                                    </div>
-                                    {/* close gallery item */}
-                                    {/* gallery item */}
-                                    <div className="item col-md-4 mb30 gallery-item illustration">
-                                        <a className="picframe" href="images/portfolio/3.jpg" title="Elaxo" data-source="https://themeforest.net/item/elaxo-app-and-software-website-template/29226060">
-                                            <span className="overlay">
-                                                <span className="d-title">Elaxo</span>
-                                            </span>
-                                            <img src="images/portfolio/3.jpg" alt="" />
-                                        </a>
-                                    </div>
-                                    {/* close gallery item */}
-                                    {/* gallery item */}
-                                    <div className="item col-md-4 mb30 gallery-item photography illustration mobile">
-                                        <a className="picframe" href="images/portfolio/4.jpg" title="Jonna" data-source="https://themeforest.net/item/jonna-personal-portfolio-website-template/26361888">
-                                            <span className="overlay">
-                                                <span className="d-title">Jonna</span>
-                                            </span>
-                                            <img src="images/portfolio/4.jpg" alt="" />
-                                        </a>
-                                    </div>
-                                    {/* close gallery item */}
-                                    {/* gallery item */}
-                                    <div className="item col-md-4 mb30 gallery-item photography mobile website">
-                                        <a className="picframe" href="images/portfolio/5.jpg" title="Justica" data-source="https://themeforest.net/item/justica-lawyer-and-attorney-website-template/29485331">
-                                            <span className="overlay">
-                                                <span className="d-title">Justica</span>
-                                            </span>
-                                            <img src="images/portfolio/5.jpg" alt="" />
-                                        </a>
-                                    </div>
-                                    {/* close gallery item */}
-                                    {/* gallery item */}
-                                    <div className="item col-md-4 mb30 gallery-item mobile website">
-                                        <a className="picframe" href="images/portfolio/6.jpg" title="Bluetec" data-source="https://themeforest.net/item/bluetec-saas-it-software-startup-landing-page-template/27106031">
-                                            <span className="overlay">
-                                                <span className="d-title">Bluetec</span>
-                                            </span>
-                                            <img src="images/portfolio/6.jpg" alt="" />
                                         </a>
                                     </div>
                                     {/* close gallery item */}
